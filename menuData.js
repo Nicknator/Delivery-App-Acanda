1b8ec2e2780caa0dde8dbc06ca24a19381697531
Nick Ezenwa <nickezenwa38@gmail.com>
2026-01-22 10:59:56 +0100

    Initial commit mit dataMenu

---
+++ menuData.js
@@ -0,0 +1,129 @@
+let dataMenu = [
+    {
+        category: "Street Food",
+        dish: [
+            {
+                name: "Suya",
+                description: ["Würzig gegrilltes Fleisch mit Zwiebeln, Tomaten und Erdnussgewürz."],
+                price: 15.56,
+                image: "1suya.png"
+
+            }, {
+                name: "Baozi-Puff",
+                description: ["Gedämpfte Brötchen mit saftiger Fleisch- oder Gemüsefüllung."],
+                price: 12.99,
+                image: "2-baozi.png"
+
+            },
+            {
+                name: "Arepas",
+                description: ["Maisfladen, gefüllt mit Käse, Fleisch, Bohnen oder Avocado."],
+                price: 12.34,
+                image: "3-arepas.jpg"
+            },
+            {
+                name: "Takoyaki",
+                description: ["Knusprige Teigbällchen mit Oktopus, Sauce und Bonito-Flocken serviert."],
+                price: 9.50,
+                image: "4-takoyaki.jpg"
+
+            },
+            {
+                name: "Tacos al Pastor",
+                description: ["Weiche Maistortillas gefüllt mit mariniertem Schweinefleisch, Ananas und Zwiebeln."],
+                price: 11.75,
+                image: "5-tacos-al-pastor.jpg"
+
+            }
+        ]
+    },
+    {
+        category: "Bowls",
+        dish: [
+            {
+                name: "Poké Bowl",
+                description: ["Frischer roher Fisch mit Reis, Avocado und Sesam."],
+                price: 18.99,
+                image: "6-poke-bowl.jpg"
+            },
+            {
+                name: "Buddha Bowl",
+                description: ["Bunte Gemüsevielfalt mit Quinoa, Kichererbsen und cremigem Dressing."],
+                price: 16.50,
+                image: "7-buddha-bowl.png"
+            },
+
+            {
+                name: "Teriyaki Chicken Bowl",
+                description: ["Saftiges Hähnchen mit Teriyaki-Sauce, Reis und knackigem Gemüse."],
+                price: 17.25,
+                image: "8-teriyaki-chicken-bowl.jpg"   
+
+            },
+
+            {
+                name: "Mexican Burrito Bowl",
+                description: ["Reis, Bohnen, würziges Fleisch, Guacamole und frische Salsa."],
+                price: 15.80,
+                image: "9-mexican-burrito-bowl.jpg"
+            },
+
+            {
+                name: "Korean Bibimbap Bowl",
+                description: ["Reis mit Gemüse, Rindfleisch, Ei und scharfer Gochujang-Sauce."],
+                price: 19.00,
+                image: "10-korean-bibimbap-bowl.jpg"
+            }     
+        ]
+
+    },
+    {
+        category: "Bürger",
+        dish: [
+            {
+                name: "Krabbenburger",
+                description: ["Saftiger Krabbenpatty mit Käse, Salat, Gurken, Zwiebeln, Tomaten."],
+                price: 14.99,
+                image: "11-krabbenburger.jpg"
+
+
+            },
+            {
+                name: "Sun Supreme Burger",
+                description: ["Saftiger Fleischpatty mit Ei, Käse und frischem Gemüse"],
+                price: 13.75,
+                image: "12-sun-supreme-burger.jpg",
+
+
+            },
+            {
+                name: "Cheeseburger ",
+                description: ["Saftiges Rindfleisch mit geschmolzenem Käse, frisch und lecker."],
+                price: 12.50,
+                image: "13-veggie-delight-burger.jpg",
+            },
+            {
+                name: "BBQ Bacon Burger",
+                description: ["Rindfleischpatty mit knusprigem Bacon und rauchiger BBQ-Sauce."],
+                price: 15.20,
+                image: "14-bbq-bacon-burger.jpg",
+            },
+            {
+                name: "Plantonburger",
+                description: ["Grüner Algenburger, würzig, saftig und appetitlich serviert"],
+                price: 16.45,
+                image: "15-plantonburger.png",
+            }
+
+
+        ]
+
+    }
+
+
+    
+
+
+
+
+]   
