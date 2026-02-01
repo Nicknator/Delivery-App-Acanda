f20e85b8ffdbda663efa55dff2d73f0f07d41c03
Nick Ezenwa <nickezenwa38@gmail.com>
2026-02-01 17:26:38 +0100

    Initial commit

--- menuData.js
+++ menuData.js
@@ -5,33 +5,33 @@ let dataMenu = [
             {
                 name: "Suya",
                 description: ["Würzig gegrilltes Fleisch mit Zwiebeln, Tomaten und Erdnussgewürz."],
-                price: 15.56,
+                price: 1,
                 image: "1-suya.png"
 
             }, {
                 name: "Baozi-Puff",
                 description: ["Gedämpfte Brötchen mit saftiger Fleisch- oder Gemüsefüllung."],
-                price: 12.99,
+                price: 1,
                 image: "2-baozi.png"
 
             },
             {
                 name: "Arepas",
                 description: ["Maisfladen, gefüllt mit Käse, Fleisch, Bohnen oder Avocado."],
-                price: 12.34,
+                price: 1,
                 image: "3-arepas.png"
             },
             {
                 name: "Takoyaki",
                 description: ["Knusprige Teigbällchen mit Oktopus, Sauce und Bonito-Flocken serviert."],
-                price: 9.50,
+                price: 1,
                 image: "4-takoyaki.png"
 
             },
             {
                 name: "Tacos al Pastor",
                 description: ["Weiche Maistortillas gefüllt mit mariniertem Schweinefleisch, Ananas und Zwiebeln."],
-                price: 11.75,
+                price: 1,
                 image: "5-taco-al-pastor.png"
 
             }
@@ -43,20 +43,20 @@ let dataMenu = [
             {
                 name: "Poké Bowl",
                 description: ["Frischer roher Fisch mit Reis, Avocado und Sesam."],
-                price: 18.99,
+                price: 2,
                 image: "6-poke-bowl.png"
             },
             {
                 name: "Buddha Bowl",
                 description: ["Bunte Gemüsevielfalt mit Quinoa, Kichererbsen und cremigem Dressing."],
-                price: 16.50,
+                price: 2,
                 image: "7-buddha-bowl.png"
             },
 
             {
                 name: "Teriyaki Chicken Bowl",
                 description: ["Saftiges Hähnchen mit Teriyaki-Sauce, Reis und knackigem Gemüse."],
-                price: 17.25,
+                price: 2,
                 image: "8-teriyaki-chicken-bowl.png"   
 
             },
@@ -64,14 +64,14 @@ let dataMenu = [
             {
                 name: "Mexican Burrito Bowl",
                 description: ["Reis, Bohnen, würziges Fleisch, Guacamole und frische Salsa."],
-                price: 15.80,
+                price: 2,
                 image: "9-mexican-burrito-bowl.png"
             },
 
             {
                 name: "Korean Bibimbap Bowl",
                 description: ["Reis mit Gemüse, Rindfleisch, Ei und scharfer Gochujang-Sauce."],
-                price: 19.00,
+                price: 2,
                 image: "10-korean-bibimbap-bowl.png"
             }     
         ]
@@ -83,7 +83,7 @@ let dataMenu = [
             {
                 name: "Krabbenburger",
                 description: ["Saftiger Krabbenpatty mit Käse, Salat, Gurken, Zwiebeln, Tomaten."],
-                price: 14.99,
+                price: 3,
                 image: "11-krabbenburger.png"
 
 
@@ -91,7 +91,7 @@ let dataMenu = [
             {
                 name: "Sun Supreme Burger",
                 description: ["Saftiger Fleischpatty mit Ei, Käse und frischem Gemüse"],
-                price: 13.75,
+                price: 3,
                 image: "12-sun-supreme-burger.png",
 
 
@@ -99,19 +99,19 @@ let dataMenu = [
             {
                 name: "Cheeseburger ",
                 description: ["Saftiges Rindfleisch mit geschmolzenem Käse, frisch und lecker."],
-                price: 12.50,
+                price: 3,
                 image: "13-chesseburger.png",
             },
             {
                 name: "BBQ Bacon Burger",
                 description: ["Rindfleischpatty mit knusprigem Bacon und rauchiger BBQ-Sauce."],
-                price: 15.20,
+                price: 3,
                 image: "14-bacon-bbq-burger.png",
             },
             {
                 name: "Plantonburger",
                 description: ["Grüner Algenburger, würzig, saftig und appetitlich serviert"],
-                price: 16.45,
+                price: 3,
                 image: "15-plantonburger.png",
             }
 
--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-
+let basket = [];
+let deliveryCosts = 5;
+renderMenu();
+renderBasket();
 
 function renderMenu() {
     let menuContent = document.getElementById("menuContent");
     menuContent.innerHTML = "";
 
+    
     dataMenu.forEach(category => {
-        console.log(category.category);
+        // console.log(category.category);
         menuContent.innerHTML += ` <h2>${category.category}</h2>`
 
 
         category.dish.forEach(dish => {
-            console.log(dish.name)
+            // console.log(dish.price);
             menuContent.innerHTML += ` 
 
         <div class="menuTemplate">
             <div>
-                <img src="img/${dish.image}" alt="${dish.name}" width="150">
+                <img src="img/${dish.image}" alt="${dish.name}">
             </div>
 
             <div class="contentPosition">
@@ -24,26 +28,98 @@ function renderMenu() {
                     <p class="price">${dish.price} €</p>
                 </div>
                 <p class="description">${dish.description}</p>
+                <button onclick="addBasket('${dish.name}', ${dish.price})">+</button>
+
             </div>
         </div>
-
-            
-            
-            
-            
-            
-            
+   
             `
-
-
         });
-
-
-
-
 
     });
 }
 
 
-renderMenu();
+
+function addBasket(name, price) {
+    let foundObject = basket.find(item => item.name === name);
+    if (foundObject) {
+        foundObject.amount++;
+
+    } else {
+        basket.push({
+            name: name,
+            price: price,
+            amount: 1
+        })
+
+    }
+    renderBasket();
+}
+
+
+function renderBasket() {
+    let basketField = document.getElementById("basketField");
+    basketField.innerHTML = "<h2>Dein Warenkorb</h2>";
+    
+
+    let sum = 0;
+
+    basket.forEach((item) => {
+        let totalPrice = item.price * item.amount;
+        sum += totalPrice;
+        
+        basketField.innerHTML += `
+        <div class="basket-item">
+            <button onclick="minusBasket('${item.name}')">-</button>
+            <button onclick="addBasket('${item.name}', ${item.price})">+</button>
+            <span>${item.amount}x ${item.name}</span>
+            <span>${totalPrice.toFixed(2)} €</span>
+        </div>`;
+    });
+
+    if (basket.length > 0) {
+        // Erst hier, wenn wir wissen wie hoch 'sum' ist, rechnen wir die Endsumme aus
+        let totalSum = sum + deliveryCosts;
+
+        basketField.innerHTML += `
+        
+           
+            <span><strong>Zwischensumme: ${sum.toFixed(2)} €</strong></span>
+            <span><strong>Lieferkosten: ${deliveryCosts.toFixed(2)} €</strong></span><br>
+            <hr>
+            <p><strong>Gesamtbetrag: ${totalSum.toFixed(2)} €</strong></p>
+            <div>
+            <button> Jetzt bestellen (${totalSum.toFixed(2)}€)</button>
+            </div>
+
+
+
+        `;
+    }
+}
+
+
+
+
+
+
+function minusBasket(name) {
+    let foundObject = basket.find(item => item.name === name);
+
+    if (foundObject) {
+
+        if (foundObject.amount > 1) {
+            foundObject.amount--;
+        }
+
+        else {
+            // Das hier muss INS erste if, aber ins ELSE vom amount > 1
+            let index = basket.indexOf(foundObject);
+            basket.splice(index, 1);
+        }
+        renderBasket();
+
+    }
+}
+
