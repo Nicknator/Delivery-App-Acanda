70dab184d589942e14afd339ea35798cc8a1bcc2
Nick Ezenwa <nickezenwa38@gmail.com>
2026-02-02 09:54:49 +0100

    Bestell Funktion mit Dialog hinzugefügt

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function renderMenu() {
     let menuContent = document.getElementById("menuContent");
     menuContent.innerHTML = "";
 
-    
+
     dataMenu.forEach(category => {
         // console.log(category.category);
         menuContent.innerHTML += ` <h2>${category.category}</h2>`
@@ -60,15 +60,15 @@ function addBasket(name, price) {
 
 function renderBasket() {
     let basketField = document.getElementById("basketField");
-    basketField.innerHTML = "<h2>Dein Warenkorb</h2>";
-    
+    basketField.innerHTML = `<h2>Dein Warenkorb</h2>`;
+
 
     let sum = 0;
 
     basket.forEach((item) => {
         let totalPrice = item.price * item.amount;
         sum += totalPrice;
-        
+
         basketField.innerHTML += `
         <div class="basket-item">
             <button onclick="minusBasket('${item.name}')">-</button>
@@ -81,6 +81,7 @@ function renderBasket() {
     if (basket.length > 0) {
         // Erst hier, wenn wir wissen wie hoch 'sum' ist, rechnen wir die Endsumme aus
         let totalSum = sum + deliveryCosts;
+        console.log(totalSum)
 
         basketField.innerHTML += `
         
@@ -90,13 +91,37 @@ function renderBasket() {
             <hr>
             <p><strong>Gesamtbetrag: ${totalSum.toFixed(2)} €</strong></p>
             <div>
-            <button> Jetzt bestellen (${totalSum.toFixed(2)}€)</button>
+            <button onclick="orderButton('')"> Jetzt bestellen (${totalSum.toFixed(2)} €)</button>
             </div>
 
 
 
         `;
     }
+}
+
+
+function orderButton() {
+    dataMenu.splice(0, dataMenu.length);
+    basketField.innerHTML = 
+    `<h2>Dein Warenkorb</h2>
+    <dialog open>
+            <h2>🎉🎆🚚 </h2>
+            <h2>Glückwunsch Bestellung bestätigt!</h2>
+            <p> Dein Essen ist auf dem Weg!</p>
+            <form method="dialog">
+                <button>OK</button>
+            </form>
+        </dialog>
+   
+    
+    `;
+
+
+    console.log(dataMenu); // Ergebnis: []
+
+
+
 }
 
 
