98eb7ea0d23cf46c340df66ca7a86a43f13af31b
Nick Ezenwa <nickezenwa38@gmail.com>
2026-02-10 07:00:45 +0100

    Code optimiertt.

--- menuData.js
+++ menuData.js
@@ -6,33 +6,33 @@ let dataMenu = [
             {
                 name: "Suya",
                 description: ["Würzig gegrilltes Fleisch mit Zwiebeln, Tomaten und Erdnussgewürz."],
-                price: 1,
+                price: 14.9,
                 image: "1-suya.png"
 
             }, {
                 name: "Baozi-Puff",
                 description: ["Gedämpfte Brötchen mit saftiger Fleisch- oder Gemüsefüllung."],
-                price: 1,
+                price: 13.5,
                 image: "2-baozi.png"
 
             },
             {
                 name: "Arepas",
                 description: ["Maisfladen, gefüllt mit Käse, Fleisch, Bohnen oder Avocado."],
-                price: 1,
+                price: 15.5,
                 image: "3-arepas.png"
             },
             {
                 name: "Takoyaki",
                 description: ["Knusprige Teigbällchen mit Oktopus, Sauce und Bonito-Flocken serviert."],
-                price: 1,
+                price: 20.9,
                 image: "4-takoyaki.png"
 
             },
             {
                 name: "Tacos al Pastor",
                 description: ["Weiche Maistortillas gefüllt mit mariniertem Schweinefleisch, Ananas und Zwiebeln."],
-                price: 1,
+                price: 13.4,
                 image: "5-taco-al-pastor.png"
 
             }
@@ -45,20 +45,20 @@ let dataMenu = [
             {
                 name: "Poké Bowl",
                 description: ["Frischer roher Fisch mit Reis, Avocado und Sesam."],
-                price: 2,
+                price: 18.9,
                 image: "6-poke-bowl.png"
             },
             {
                 name: "Buddha Bowl",
                 description: ["Bunte Gemüsevielfalt mit Quinoa, Kichererbsen und cremigem Dressing."],
-                price: 2,
+                price: 14.9,
                 image: "7-buddha-bowl.png"
             },
 
             {
                 name: "Teriyaki Chicken Bowl",
                 description: ["Saftiges Hähnchen mit Teriyaki-Sauce, Reis und knackigem Gemüse."],
-                price: 2,
+                price: 12.9,
                 image: "8-teriyaki-chicken-bowl.png"   
 
             },
@@ -66,14 +66,14 @@ let dataMenu = [
             {
                 name: "Mexican Burrito Bowl",
                 description: ["Reis, Bohnen, würziges Fleisch, Guacamole und frische Salsa."],
-                price: 2,
+                price: 22.7,
                 image: "9-mexican-burrito-bowl.png"
             },
 
             {
                 name: "Korean Bibimbap Bowl",
                 description: ["Reis mit Gemüse, Rindfleisch, Ei und scharfer Gochujang-Sauce."],
-                price: 2,
+                price: 19.99,
                 image: "10-korean-bibimbap-bowl.png"
             }     
         ]
@@ -86,7 +86,7 @@ let dataMenu = [
             {
                 name: "Krabbenburger",
                 description: ["Saftiger Krabbenpatty mit Käse, Salat, Gurken, Zwiebeln, Tomaten."],
-                price: 3,
+                price: 11.9,
                 image: "11-krabbenburger.png"
 
 
@@ -94,7 +94,7 @@ let dataMenu = [
             {
                 name: "Sun Supreme Burger",
                 description: ["Saftiger Fleischpatty mit Ei, Käse und frischem Gemüse"],
-                price: 3,
+                price: 14.9,
                 image: "12-sun-supreme-burger.png",
 
 
@@ -102,19 +102,19 @@ let dataMenu = [
             {
                 name: "Cheeseburger ",
                 description: ["Saftiges Rindfleisch mit geschmolzenem Käse, frisch und lecker."],
-                price: 3,
+                price: 15.5,
                 image: "13-chesseburger.png",
             },
             {
                 name: "BBQ Bacon Burger",
                 description: ["Rindfleischpatty mit knusprigem Bacon und rauchiger BBQ-Sauce."],
-                price: 3,
+                price: 12.5,
                 image: "14-bacon-bbq-burger.png",
             },
             {
                 name: "Plantonburger",
                 description: ["Grüner Algenburger, würzig, saftig und appetitlich serviert"],
-                price: 3,
+                price: 8.9,
                 image: "15-plantonburger.png",
             }
 
